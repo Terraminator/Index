d01a12b0be7af6f47a9b45f769393b4abeb75761
Terraminator <erik1wilhelm@gmail.com>
2021-11-26 15:16:30 +0100

    y

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ class IndexSpider:
 	def __init__(self):
 		self.man = man_db.man()
 		self.urls = [
-			'https://www.wikipedia.com',
+			#'https://www.wikipedia.com',
+			#'https://en.wikipedia.com/wiki/Wikipedia:Contents/A%E2%80%93Z_index'
+			#'https://www.google.com',
+			'https://www.perspektiven-finden.com/unternehmen',
 			]
 		self.start_requests()
 	
@@ -26,27 +29,26 @@ class IndexSpider:
 			res = requests.get(url)
 			soup = bs4.BeautifulSoup(res.text, "lxml")
 			for link in soup.findAll('a'):
-				link = link.get('href')
-				if not link in self.urls:
+				try:
+					link = link.get('href')
 					if not "http" in link:
-						if link =="/%s":
+						if link[0] == '/':
 							link = url.split(".%s/")[0] + url.split(".com/")[1] + link
 						else:
 							link = url.split("//")[0] + link
-					self.urls.append(link)
-					print(link)
+					if not link in self.urls:
+						self.urls.append(link)
+				except:
+					pass
 
 		except:
 			pass
 
 			
 	def parse(self, url):
-		#try:
 		self.get_links(url)
 		if "http" in url:
 			self.man.write(url)
-		#except:
-		#	pass
 
 if __name__ == "__main__":
 	indexspider = IndexSpider()
